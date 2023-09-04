92e5e5010bd3ff9a72ef8b2b1ad41b6596074292
Bewin007 <bewinfelix@karunya.edu.in>
2023-09-04 14:37:50 +0530

    Tested

--- backend/ecertificate/api/views.py
+++ backend/ecertificate/api/views.py
@@ -29,17 +29,17 @@ class Admin_api(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, pk):
-        try:
-            certificate = Certificate.objects.get(pk=pk)
-        except Certificate.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+    # def put(self, request, pk):
+    #     try:
+    #         certificate = Certificate.objects.get(pk=pk)
+    #     except Certificate.DoesNotExist:
+    #         return Response(status=status.HTTP_404_NOT_FOUND)
 
-        serializer = CertificateSerializer(certificate, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #     serializer = CertificateSerializer(certificate, data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         try:
@@ -49,6 +49,8 @@ class Admin_api(APIView):
 
         certificate.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 
 
 class User_api(APIView):
